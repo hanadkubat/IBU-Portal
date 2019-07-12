5b7ecad853eade94eb4e0e6bf922c398c5b5a267
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-07-12 13:48:18 +0200

    AD b2c set up

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ## Mibo communications private portal
-
-//https://medium.com/@fiqriismail/how-to-secure-your-reactjs-frontend-with-azure-ad-b2c-8fd165f602e8
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+//global bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//active directory setup
+/*import b2cauth from "react-azure-adb2c";
+b2cauth.initialize({
+  instance: "https://login.microsoftonline.com/tfp/",
+  tenant: "skikaportal.onmicrosoft.com",
+  signInPolicy: "B2C_1_react_signup",
+  applicationId: "415e7c68-fa05-4a25-8d84-a8a94f8f9afb",
+  cacheLocation: "sessionStorage",
+  scopes: ["https://skikaportal.onmicrosoft.com/api/user_impersonation"],
+  redirectUri: "http://localhost:3000",
+  postLogoutRedirectUri: window.location.origin
+});
+
+b2cauth.run(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+  serviceWorker.unregister();
+});*/
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
