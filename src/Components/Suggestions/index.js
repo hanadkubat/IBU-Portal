e865d01a511c641dcc53c7e178963647c9221e1f
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-07-15 09:08:48 +0200

    dashboard overhaul

---
+++ src/Components/Suggestions/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Suggestions extends Component {
+    render() {
+        return (
+            <div>
+                Prijedlozi
+            </div>
+        )
+    }
+}
---
+++ src/app/Dashboard/listItems.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+import {Link} from 'react-router-dom';
+
+const linkStyle = {
+  textDecoration : 'none',
+  color : 'black'
+}
+
+export const mainListItems = (
+  <div>
+    <Link to="/dashboard" style={linkStyle}>
+    <ListItem button>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Prijedlozi" />
+    </ListItem>
+    </Link>
+    <Link to="/dashboard/news" style={linkStyle}>
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Novosti" />
+      </ListItem>
+    </Link>
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Odjava korisnika" />
+    </ListItem>
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListSubheader inset>Administracija</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItem>
+  </div>
+);
