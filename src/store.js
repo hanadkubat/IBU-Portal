674f6b144b8526c5100d1833184930020942076c
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-11-09 14:13:30 +0100

    store update

--- src/store.js
+++ src/store.js
@@ -4,13 +4,13 @@ import reducers from "./reducers";
 
 const middleware = applyMiddleware(thunk);
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+//const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(
     reducers,
     compose(
         middleware,
-        devTools
+        //devTools
     )
 );
 
