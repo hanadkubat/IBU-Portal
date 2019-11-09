f2d7f0a8c3b8b8f179d50e411028c62d3724fc79
Hanad Kubat <34318441+hanadkubat@users.noreply.github.com>
2019-11-09 13:05:54 +0100

    tenant change

--- src/config/adalConfig.js
+++ src/config/adalConfig.js
@@ -2,10 +2,13 @@ import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
 import * as jwtDecode from 'jwt-decode';
  
 export const adalConfig = {
-  tenant: '1b59fec5-1d4e-4835-8dc9-41f3e5a6ae9d',
-  clientId: '7c60b2af-e970-4c05-8278-67564929e213',
+  // tenant: '1b59fec5-1d4e-4835-8dc9-41f3e5a6ae9d',
+  // clientId: '7c60b2af-e970-4c05-8278-67564929e213',
+  tenant: '93838cca-6334-421f-83b8-8727c93326e3',
+  clientId: '0bd686c6-1d88-4ae5-9112-055adc3e6874',
   endpoints: {
-    api: '00000002-0000-0000-c000-000000000000',
+    // api: '00000002-0000-0000-c000-000000000000',
+    api: '00000003-0000-0000-c000-000000000000',
   },
   //'http://localhost:3000'
   postLogoutRedirectUri: 'https://ibu-it-portal.herokuapp.com',
